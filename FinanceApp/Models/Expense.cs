7221e98de364fac5bc3108258641d913bd3ebba0
Piotr <piotr.paluszkiewicz.kontakt@gmail.com>
2025-06-05 16:49:18 +0200

    Add project files.

---
+++ FinanceApp/Models/Expense.cs
@@ -0,0 +1,11 @@
+﻿namespace FinanceApp.Models
+{
+    public class Expense
+    {
+        public int Id { get; set; }
+        public string Description { get; set; } = null!;
+
+
+
+    }
+}
---
+++ FinanceApp/Readme.md
@@ -0,0 +1,1 @@
+https://www.youtube.com/watch?v=QtiM87MV27w
