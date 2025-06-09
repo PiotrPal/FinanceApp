5e3141c8e2ab455d2e885728c0aa8556939fcc40
Piotr <piotr.paluszkiewicz.kontakt@gmail.com>
2025-06-09 14:46:06 +0200

    Create Method

--- FinanceApp/Models/Expense.cs
+++ FinanceApp/Models/Expense.cs
@@ -8,9 +8,9 @@ namespace FinanceApp.Models
         [Required]
         public string Description { get; set; } = null!;
         [Required]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Kwota w niepoprawnym zakresie (0.01)-dużo ")]
         public double Amount { get; set; }
         [Required]
-        [Range(0.01,double.MaxValue,ErrorMessage = "Kwota w niepoprawnym zakresie (0.01)-dużo ")]
         public string Category { get; set; } = null!;
         public DateTime Date { get; set; } = DateTime.Now;
     }
